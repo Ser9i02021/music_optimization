41f55e1ee4a2bf72e53474a33c56f96eaf4fcc74
Ser9i02021 <78425749+Ser9i02021@users.noreply.github.com>
2025-03-01 00:48:55 -0300

    Update cost_matrix_contruction.py

--- cost_matrix_contruction.py
+++ cost_matrix_contruction.py
@@ -1,5 +1,92 @@
 import numpy as np
 from lxml import etree
+from label_chosen_licks import select_N_lick_samples 
+
+
+licks_list = select_N_lick_samples(5)
+
+print(licks_list)
+
+cost_matrix = np.full((4, 4), 100)
+
+for i in range(len(licks_list)):
+    for j in range(len(licks_list)):
+        if i != j:
+            # T1
+            if "C1" in licks_list[i][2] and ("C1" not in licks_list[j][2]):
+                cost_matrix[i][j] = -50
+                print("cost_matrix[{}][{}]: T1".format(i, j))
+            # T2
+            if not (("C2" in licks_list[i][2]) or ("C3" in licks_list[i][2]) or ("C4" in licks_list[i][2])) and ("C2" in licks_list[j][2]):
+                cost_matrix[i][j] = -15
+                print("cost_matrix[{}][{}]: T2".format(i, j))
+            # T3
+            if ("C2" in licks_list[i][2]) and ("C5" in licks_list[j][2]) or ("C6" in licks_list[j][2]) or ("C7" in licks_list[j][2]):
+                cost_matrix[i][j] = -15
+                print("cost_matrix[{}][{}]: T3".format(i, j))
+            # T4
+            if (((ord(licks_list[i][1][0]) - 2) % 7) == ((ord(licks_list[i][1][0]) - 3) % 7)) and  licks_list[i][1][1] == licks_list[j][1][1]:
+                cost_matrix[i][j] = -15
+                print("cost_matrix[{}][{}]: T4".format(i, j))
+            # T5
+            if licks_list[i][1] == licks_list[j][0]:
+                cost_matrix[i][j] = -15
+                print("cost_matrix[{}][{}]: T5".format(i, j))
+            # T6
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Load XML file
@@ -7,11 +94,11 @@ tree = etree.parse(r"C:\Users\pc\Documents\TCC_I_dset\licks_dset_new\Turnaround_
 root = tree.getroot()
 
 # Print root tag
-print("Root tag:", root.tag)
+#print("Root tag:", root.tag)
 
 # Finding elements using XPath
-for element in root.xpath("//item"):  # Adjust the XPath to your XML structure
-    print("Item:", element.text)
+#for element in root.xpath("//item"):  # Adjust the XPath to your XML structure
+#    print("Item:", element.text)
 
 P = np.full((5, 5), 100)  
 
@@ -35,3 +122,5 @@ def assign_a_value_for_one_element():
             elif i == "ends with pause <= 1 beat" and j == "starts with pause":
                 P[i][j] = -15
                 return
+            else:
+                pass
